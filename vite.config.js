4712edd963950e101770e4737eada0cb83877d51
JonathanFLF <ferney5585@gmail.com>
2024-02-12 23:31:39 -0500

    cambio

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://JonathanFLF.github.io/February-Declaration/",
+  base: "https://martl7.github.io/February-Declaration/",
 })
